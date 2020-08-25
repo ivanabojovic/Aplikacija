a911b84eb68bc9188560e7ee9ffdc25a486bc580
Ivana Bojovic <ivana.bojovic.16@singimail.rs>
2020-08-25 18:18:19 +0200

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller()
+export class AppController {
+  
+
+  @Get() // http://localhost:3000/
+  getHello(): string {
+    return 'Hello World';
+  }
+}
